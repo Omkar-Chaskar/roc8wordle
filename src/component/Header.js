58ac9a1481ec04088df2e03925f519cb4dbcac85
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2023-01-25 21:48:02 +0530

    feat : added theme feature
    added theme feature and also win and lose model

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import "../style/header.css";
+import { theme , handleTheme } from "../features/wordle/wordleSlice";
+import { useSelector , useDispatch } from 'react-redux';
 
 function Header() {
+
+  const currTheme = useSelector(theme);
+
+  const dispatch = useDispatch();
+
   return (
     <div className='header'>
         <div className='header title'>
             <h1>WORDEL</h1>
         </div>
         <div className='header toggle'>
-            <label className="switch">
-                <input type="checkbox" defaultChecked />
-                <span className="slider round"></span>
-            </label>
+          {currTheme === "light" ? 
+            <button className='dark-mode button' onClick={()=>dispatch(handleTheme())}>Dark Mode</button>
+            : 
+            <button className='light-mode button' onClick={()=>dispatch(handleTheme())}>Ligth Mode</button>
+          }
         </div>
     </div>
   )
--- src/features/wordle/wordleSlice.js
+++ src/features/wordle/wordleSlice.js
@@ -5,6 +5,9 @@ const initialState = {
   word: '',
   wordle: [],
   currAttempt: 0,
+  won: false,
+  loss: false,
+  theme: "light",
 };
 
 export const wordleSlice = createSlice({
@@ -13,7 +16,10 @@ export const wordleSlice = createSlice({
   reducers: {
     getWord : (state) => {
       state.word = words[Math.floor(Math.random() * words.length)]; 
-      state.wordle = new Array(6).fill('')
+      state.wordle = new Array(6).fill('');
+      state.currAttempt = 0;
+      state.won= false;
+      state.loss= false;
     },
     handleKeySelect: (state, action) => {
       if(state.wordle[state.currAttempt].length < 5){
@@ -23,22 +29,35 @@ export const wordleSlice = createSlice({
     handleEnter: (state) => {
       if(state.wordle[state.currAttempt].length === 5){
         if(words.includes(state.wordle[state.currAttempt].toLowerCase())){
+          if(state.wordle[state.currAttempt].toLowerCase() === state.word){
+            state.won = true;
+          }
           state.currAttempt += 1;
+        }
+        if(state.currAttempt === 6){
+          state.loss = true;
         }
       }
     },
     handleDelete: (state) => {
       state.wordle[state.currAttempt] = state.wordle[state.currAttempt].slice(0,state.wordle[state.currAttempt].length - 1)
-    }
+    },
+    handleTheme: (state) => {
+      console.log(state.theme);
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
   },
   extraReducers: (builder) => {
   },
 });
 
-export const { handleKeySelect, handleEnter, handleDelete, getWord } = wordleSlice.actions;
+export const { handleKeySelect, handleEnter, handleDelete, getWord , handleTheme } = wordleSlice.actions;
 
 export const selectWordle = (state) => state.wordle.wordle;
 export const getNewWord = (state) => state.wordle.word;
 export const currAttempt = (state) => state.wordle.currAttempt;
+export const won = (state) => state.wordle.won;
+export const loss = (state) => state.wordle.loss;
+export const theme = (state) => state.wordle.theme;
 
 export default wordleSlice.reducer;
